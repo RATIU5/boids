c43cad969ddd6ec9724695cb365b8f34b513c5dc
RATIU5 <jcmemmott20@outlook.com>
2023-06-08 09:51:59 -0600

    constructor

--- js/boid.js
+++ js/boid.js
@@ -3,6 +3,17 @@ class Boid {
     vel = createVector(0, 0);
     acc = createVector(0, 0);
     perceptionRadius = 30;
+    color = color(255);
+    scale = 1;
+
+    constructor(boid) {
+        this.pos = boid.pos;
+        this.vel = boid.vel;
+        this.acc = boid.acc;
+        this.perceptionRadius = boid.perceptionRadius;
+        this.color = boid.color;
+        this.scale = boid.scale;
+    }
 
     draw() {
         fill(30, 40, 100);
