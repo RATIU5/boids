9b60a9b60025afccfa1b7015082a918a95f46806
RATIU5 <jcmemmott20@outlook.com>
2023-06-08 09:42:26 -0600

    delete old stretching shapes

--- js/app.js
+++ js/app.js
@@ -1,20 +1,17 @@
-let i = 0;
-
 function setup() {
     createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-    i++;
-    background(100, 200, 150);
-
-    fill(100, 250, 5);
-    ellipse(i, 300, 100, i);
+    background(10);
 }
 
 function spawnBoids(amount) {
     let flock = [];
     for (let i = 0; i < amount; i++) {
         const boid = new Boid();
+        flock.push(boid);
     }
+
+    return flock;
 }
