356ab281ff7cd41203c8c9ba1146b534b80d1499
RATIU5 <jcmemmott20@outlook.com>
2023-06-08 09:08:25 -0600

    movement baby

--- js/app.js
+++ js/app.js
@@ -1,10 +1,13 @@
+let i = 0;
+
 function setup() {
     createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
+    i++;
     background(100, 200, 150);
 
-    fill(100, 100, 255);
-    ellipse(300, 300, 100, 100);
+    fill(100, 250, 5);
+    ellipse(i, 300, 100, i);
 }
