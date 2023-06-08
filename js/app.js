9800e1db447a4582285f1c76045d6d37084d7126
RATIU5 <jcmemmott20@outlook.com>
2023-06-08 08:52:40 -0600

    circle time

---
+++ js/app.js
@@ -0,0 +1,10 @@
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+    background(100, 200, 150);
+
+    fill(100, 100, 255);
+    ellipse(300, 300, 100, 100);
+}
