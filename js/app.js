a8f995902a8fa133010fff2ffbf08d06643cff16
RATIU5 <jcmemmott20@outlook.com>
2023-06-12 08:25:03 -0600

    format: formatted code

--- js/app.js
+++ js/app.js
@@ -19,57 +19,57 @@ const ALN_SPEED = 3;
 const COS_SPEED = 3;
 
 function setup() {
-    createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight);
 
-    min = createVector(0, 0);
-    max = createVector(width, height);
+  min = createVector(0, 0);
+  max = createVector(width, height);
 
-    spawnBoids(BOID_COUNT);
+  spawnBoids(BOID_COUNT);
 }
 
 function windowResized() {
-    resizeCanvas(windowWidth, windowHeight);
-    max = createVector(windowWidth, windowHeight);
+  resizeCanvas(windowWidth, windowHeight);
+  max = createVector(windowWidth, windowHeight);
 }
 
 function draw() {
-    background(50);
+  background(50);
 
-    for (const boid of flock) {
-        boid.wrap(min, max);
+  for (const boid of flock) {
+    boid.wrap(min, max);
 
-        let separationForce = boid.createBoidForce();
-        let alignmentForce = boid.createBoidForce();
-        let cohesionForce = boid.createBoidForce();
+    let separationForce = boid.createBoidForce();
+    let alignmentForce = boid.createBoidForce();
+    let cohesionForce = boid.createBoidForce();
 
-        for (const flockmate of flock) {
-            if (boid == flockmate) continue;
-            boid.assignSeparationForce(flockmate, separationForce, SEP_SIZE);
-            boid.assignAlignmentForce(flockmate, alignmentForce, ALN_SIZE);
-            boid.assignCohesionForce(flockmate, cohesionForce, COS_SIZE);
-        }
-
-        boid.applySeparationForce(separationForce, SEP_WEIGHT, SEP_SPEED);
-        boid.applyAlignmentForce(alignmentForce, ALN_WEIGHT, ALN_SPEED);
-        boid.applyCohesionForce(cohesionForce, COS_WEIGHT, COS_SPEED);
-
-        boid.update(4);
-        boid.draw();
+    for (const flockmate of flock) {
+      if (boid == flockmate) continue;
+      boid.assignSeparationForce(flockmate, separationForce, SEP_SIZE);
+      boid.assignAlignmentForce(flockmate, alignmentForce, ALN_SIZE);
+      boid.assignCohesionForce(flockmate, cohesionForce, COS_SIZE);
     }
+
+    boid.applySeparationForce(separationForce, SEP_WEIGHT, SEP_SPEED);
+    boid.applyAlignmentForce(alignmentForce, ALN_WEIGHT, ALN_SPEED);
+    boid.applyCohesionForce(cohesionForce, COS_WEIGHT, COS_SPEED);
+
+    boid.update(4);
+    boid.draw();
+  }
 }
 
 function spawnBoids(amount) {
-    for (let i = 0; i < amount; i++) {
-        const vel = p5.Vector.random2D();
-        vel.setMag(random(1, 5));
+  for (let i = 0; i < amount; i++) {
+    const vel = p5.Vector.random2D();
+    vel.setMag(random(1, 5));
 
-        const boid = new Boid({
-            pos: createVector(random(width), random(height)),
-            vel: vel,
-            perceptionRadius: PERCEPTION_RADIUS,
-            scale: SCALE,
-            color: color(random(50, 255), random(50, 255), 100),
-        });
-        flock.push(boid);
-    }
+    const boid = new Boid({
+      pos: createVector(random(width), random(height)),
+      vel: vel,
+      perceptionRadius: PERCEPTION_RADIUS,
+      scale: SCALE,
+      color: color(random(50, 255), random(50, 255), 100),
+    });
+    flock.push(boid);
+  }
 }
--- js/boid.js
+++ js/boid.js
@@ -1,157 +1,152 @@
 class Boid {
-    pos = createVector(0, 0);
-    vel = createVector(0, 0);
-    acc = createVector(0, 0);
-    perceptionRadius = 30;
-    color = color(255);
-    scale = 1;
+  pos = createVector(0, 0);
+  vel = createVector(0, 0);
+  acc = createVector(0, 0);
+  perceptionRadius = 30;
+  color = color(255);
+  scale = 1;
 
-    constructor(boid) {
-        this.pos = boid.pos;
-        this.vel = boid.vel;
-        this.perceptionRadius = boid.perceptionRadius;
-        this.color = boid.color;
-        this.scale = boid.scale;
+  constructor(boid) {
+    this.pos = boid.pos;
+    this.vel = boid.vel;
+    this.perceptionRadius = boid.perceptionRadius;
+    this.color = boid.color;
+    this.scale = boid.scale;
+  }
+
+  draw() {
+    fill(this.color);
+    noStroke();
+
+    push();
+    translate(this.pos.x, this.pos.y);
+    rotate(this.vel.heading() + PI / 2);
+    const scale = this.scale;
+    beginShape();
+    vertex(0 * scale, -7 * scale);
+    vertex(-3 * scale, 7 * scale);
+    vertex(0, 5 * scale);
+    vertex(3 * scale, 7 * scale);
+    endShape(CLOSE);
+    pop();
+
+    fill(this.color.levels[0], this.color.levels[1], this.color.levels[2], 20);
+    ellipse(this.pos.x, this.pos.y, (this.perceptionRadius * scale) / 2);
+  }
+
+  bounce() {
+    const x = this.pos.x;
+    const y = this.pos.y;
+
+    if (x < 0 || x > width) {
+      this.vel.x = this.vel.x * -1;
     }
 
-    draw() {
-        fill(this.color);
-        noStroke();
+    if (y < 0 || y > height) {
+      this.vel.y = this.vel.y * -1;
+    }
+  }
 
-        push();
-        translate(this.pos.x, this.pos.y);
-        rotate(this.vel.heading() + PI / 2);
-        const scale = this.scale;
-        beginShape();
-        vertex(0 * scale, -7 * scale);
-        vertex(-3 * scale, 7 * scale);
-        vertex(0, 5 * scale);
-        vertex(3 * scale, 7 * scale);
-        endShape(CLOSE);
-        pop();
+  wrap(min, max) {
+    const x = this.pos.x;
+    const y = this.pos.y;
+    const off = (this.perceptionRadius * this.scale) / 2;
 
-        fill(
-            this.color.levels[0],
-            this.color.levels[1],
-            this.color.levels[2],
-            20
-        );
-        ellipse(this.pos.x, this.pos.y, (this.perceptionRadius * scale) / 2);
+    if (x < min.x - off) {
+      this.pos.x = max.x + off;
+    } else if (x > max.x + off) {
+      this.pos.x = min.x - off;
     }
 
-    bounce() {
-        const x = this.pos.x;
-        const y = this.pos.y;
-
-        if (x < 0 || x > width) {
-            this.vel.x = this.vel.x * -1;
-        }
-
-        if (y < 0 || y > height) {
-            this.vel.y = this.vel.y * -1;
-        }
+    if (y < min.y - off) {
+      this.pos.y = max.y + off;
+    } else if (y > max.y + off) {
+      this.pos.y = min.y - off;
     }
+  }
 
-    wrap(min, max) {
-        const x = this.pos.x;
-        const y = this.pos.y;
-        const off = (this.perceptionRadius * this.scale) / 2;
+  applyForce(force) {
+    this.acc.add(force);
+  }
 
-        if (x < min.x - off) {
-            this.pos.x = max.x + off;
-        } else if (x > max.x + off) {
-            this.pos.x = min.x - off;
-        }
+  createBoidForce() {
+    return [createVector(0, 0), 0];
+  }
 
-        if (y < min.y - off) {
-            this.pos.y = max.y + off;
-        } else if (y > max.y + off) {
-            this.pos.y = min.y - off;
-        }
+  assignSeparationForce(flockmate, boidForce, perceptionRadius) {
+    const radius = perceptionRadius ?? this.perceptionRadius;
+    const [force] = boidForce;
+
+    let d = dist(this.pos.x, this.pos.y, flockmate.pos.x, flockmate.pos.y);
+
+    if (d < radius) {
+      const diff = p5.Vector.sub(this.pos, flockmate.pos);
+      diff.div(d);
+      force.add(diff);
+
+      boidForce[0].add(diff);
+      boidForce[1]++;
     }
+  }
 
-    applyForce(force) {
-        this.acc.add(force);
+  applySeparationForce(boidForce, maxForce = 0.2, maxSpeed = 4) {
+    const [force, count] = boidForce;
+    if (count > 0) {
+      force.div(count);
+      force.mult(maxSpeed);
+      force.sub(this.vel);
+      force.limit(maxForce);
+      this.applyForce(force);
     }
+  }
 
-    createBoidForce() {
-        return [createVector(0, 0), 0];
+  assignAlignmentForce(flockmate, boidForce, perceptionRadius) {
+    const radius = perceptionRadius ?? this.perceptionRadius;
+    let d = dist(this.pos.x, this.pos.y, flockmate.pos.x, flockmate.pos.y);
+
+    if (d < radius) {
+      boidForce[0].add(flockmate.vel);
+      boidForce[1]++;
     }
+  }
 
-    assignSeparationForce(flockmate, boidForce, perceptionRadius) {
-        const radius = perceptionRadius ?? this.perceptionRadius;
-        const [force] = boidForce;
-
-        let d = dist(this.pos.x, this.pos.y, flockmate.pos.x, flockmate.pos.y);
-
-        if (d < radius) {
-            const diff = p5.Vector.sub(this.pos, flockmate.pos);
-            diff.div(d);
-            force.add(diff);
-
-            boidForce[0].add(diff);
-            boidForce[1]++;
-        }
+  applyAlignmentForce(boidForce, maxForce = 0.2, maxSpeed = 4) {
+    const [force, count] = boidForce;
+    if (count > 0) {
+      force.div(count);
+      force.setMag(maxSpeed);
+      force.sub(this.vel);
+      force.limit(maxForce);
+      this.applyForce(force);
     }
+  }
 
-    applySeparationForce(boidForce, maxForce = 0.2, maxSpeed = 4) {
-        const [force, count] = boidForce;
-        if (count > 0) {
-            force.div(count);
-            force.mult(maxSpeed);
-            force.sub(this.vel);
-            force.limit(maxForce);
-            this.applyForce(force);
-        }
+  assignCohesionForce(flockmate, boidForce, perceptionRadius) {
+    const radius = perceptionRadius ?? this.perceptionRadius;
+    let d = dist(this.pos.x, this.pos.y, flockmate.pos.x, flockmate.pos.y);
+
+    if (d < radius) {
+      boidForce[0].add(flockmate.pos);
+      boidForce[1]++;
     }
+  }
 
-    assignAlignmentForce(flockmate, boidForce, perceptionRadius) {
-        const radius = perceptionRadius ?? this.perceptionRadius;
-        let d = dist(this.pos.x, this.pos.y, flockmate.pos.x, flockmate.pos.y);
-
-        if (d < radius) {
-            boidForce[0].add(flockmate.vel);
-            boidForce[1]++;
-        }
+  applyCohesionForce(boidForce, maxForce = 0.2, maxSpeed = 4) {
+    const [force, count] = boidForce;
+    if (count > 0) {
+      force.div(count);
+      force.sub(this.pos);
+      force.setMag(maxSpeed);
+      force.sub(this.vel);
+      force.limit(maxForce);
+      this.applyForce(force);
     }
+  }
 
-    applyAlignmentForce(boidForce, maxForce = 0.2, maxSpeed = 4) {
-        const [force, count] = boidForce;
-        if (count > 0) {
-            force.div(count);
-            force.setMag(maxSpeed);
-            force.sub(this.vel);
-            force.limit(maxForce);
-            this.applyForce(force);
-        }
-    }
-
-    assignCohesionForce(flockmate, boidForce, perceptionRadius) {
-        const radius = perceptionRadius ?? this.perceptionRadius;
-        let d = dist(this.pos.x, this.pos.y, flockmate.pos.x, flockmate.pos.y);
-
-        if (d < radius) {
-            boidForce[0].add(flockmate.pos);
-            boidForce[1]++;
-        }
-    }
-
-    applyCohesionForce(boidForce, maxForce = 0.2, maxSpeed = 4) {
-        const [force, count] = boidForce;
-        if (count > 0) {
-            force.div(count);
-            force.sub(this.pos);
-            force.setMag(maxSpeed);
-            force.sub(this.vel);
-            force.limit(maxForce);
-            this.applyForce(force);
-        }
-    }
-
-    update(maxSpeed) {
-        this.vel.add(this.acc);
-        this.pos.add(this.vel);
-        this.vel.limit(maxSpeed);
-        this.acc.mult(0);
-    }
+  update(maxSpeed) {
+    this.vel.add(this.acc);
+    this.pos.add(this.vel);
+    this.vel.limit(maxSpeed);
+    this.acc.mult(0);
+  }
 }
